7cfabfa2f3e70c3ad2bd7ea25ce8ec24e0985297
GonzaloRomero <gonzalo.romero@edunext.co>
2021-05-26 09:17:32 -0500

    Modify setup.py file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    setup_requires=[u'pbr>=5.6.0', u'setuptools>=17.1'],
+    setup_requires=[u'setuptools>=17.1'],
     python_requires=">=3.6, <3.9",
     pbr=False,
 )
